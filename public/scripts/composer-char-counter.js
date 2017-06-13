d4fc419f0501a6fe3903296f309a8a1ce64e6055
donaldma <donald.ma@hotmail.ca>
2017-06-13 20:34:12 +0000

    Added: implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".new-tweet form").keyup(function() {
+    var remaining = 140 - $(".textboxArea").val().length;
+    if(remaining < 0) {
+      $(".counter").css('color', 'red')
+    } else {
+      $(".counter").css('color', 'black')
+    }
+    $(".counter").text(remaining);
+  });
+});
