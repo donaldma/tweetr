97de5f1dcf61ecca7422201ad570924922bdc326
donaldma <donald.ma@hotmail.ca>
2017-06-14 03:00:23 +0000

    Edited hover

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,36 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+
+const tweetData =
+[
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  }
+]
+
+$(document).ready(function() {
+  var $tweet = createTweetElement(tweetData[0]);
+
+  // console.log($tweet)
+
+  $('.tweetsContainer').append($tweet);
+});
+
+// console.log(tweetData[0].user.name  )
+
+
+function createTweetElement(tweetObj) {
+  return `<article class="tweet">${tweetObj.user.name}</article>`;
+}
+
 
