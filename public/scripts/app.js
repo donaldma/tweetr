73e85a76f89037a25679c66d4631748825b3f10f
donaldma <donald.ma@hotmail.ca>
2017-06-14 22:48:18 +0000

    Added: form submission, fetching tweets, form validation, refetch tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -48,20 +48,30 @@ const tweetData =
 ]
 
 $(document).ready(function() {
+  function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+  }
 
   function createTweetElement(tweet) {
+    var tweetHandle = tweet.user.handle;
+    var tweetAvatar = tweet.user.avatars.regular;
+    var tweetName = tweet.user.name;
+    var tweetContent = escape(tweet.content.text);
+    var tweetDate = moment(tweet.created_at).fromNow();
     return `
     <article class="tweet">
       <header>
-        <span class="tweetsHandle">${tweet.user.handle}</span>
-        <img class="profileAvatar" src=${tweet.user.avatars.regular}>
-        <h2 class="tweetsName">${tweet.user.name}</h2>
+        <span class="tweetsHandle">${tweetHandle}</span>
+        <img class="profileAvatar" src=${tweetAvatar}>
+        <h2 class="tweetsName">${tweetName}</h2>
       </header>
       <p class="tweetBody">
-        ${tweet.content.text}
+        ${tweetContent}
       </p>
       <footer>
-        <span class="date">${tweet.created_at}</span>
+        <span class="date">${tweetDate}</span>
         <i class="fa fa-flag" aria-hidden="true"></i>
         <i class="fa fa-retweet" aria-hidden="true"></i>
         <i class="fa fa-heart" aria-hidden="true"></i>
@@ -70,11 +80,35 @@ $(document).ready(function() {
   };
 
   function renderTweets(tweets) {
+    $('.tweetsContainer').empty();
     for(var i = 0; i < tweets.length; i++) {
       var $tweet = createTweetElement(tweets[i]);
-      $('.tweetsContainer').append($tweet);
+      $('.tweetsContainer').prepend($tweet);
     }
   }
 
-  renderTweets(tweetData);
+  function loadTweets() {
+    $.ajax({
+      url: '/tweets/',
+      method: 'GET'
+    }).done(renderTweets)
+  }
+
+  $(".tweetForm").on("submit", function(event) {
+    event.preventDefault();
+    if($(".textboxArea").val().length > 140 || $(".textboxArea").val().length === 0) {
+      alert('fuck off');
+    } else {
+      $.ajax({
+        method: "POST",
+        url: "/tweets",
+        data: $(this).serialize()
+      }).done(function() {
+        loadTweets();
+      })
+    }
+  })
+
+  loadTweets();
+
 });
